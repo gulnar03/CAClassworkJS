5d7400fdb668391fae93b99c5c1e81160729a490
gulnar03 <gulnarbabakisiyeva@gmail.com>
2023-11-03 02:54:33 -0700

    task didn't finish

---
+++ 03.Array&Object/main.js
@@ -0,0 +1,44 @@
+// TASK 1
+
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+// let max = numbers[0];
+// let min = numbers[0];
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+//   if (numbers[i] < min) {
+//     min = numbers[i];
+//   }
+// }
+// console.log("Max: ", max, "Min: ", min);
+
+// TASK 2
+
+// let numbers = [12, 45, 6, 78, 34, 99, 23];
+// let sum = 0;
+// for (let i = 0; i < numbers.length; i++) {
+//   sum += numbers[i];
+// }
+// console.log(sum);
+
+// // TASK 3
+
+// let numbers = [1, 2, 3, 4, 5];
+// let second = numbers[3];
+// numbers[3] = numbers[2];
+// numbers[2] =second ;
+// console.log(numbers);
+
+// TASK 4
+// let numbers = [12, -5, 6, -3, 34, -1, 23];
+// let array = [];
+// for (let i = 0; i < numbers.length; i++) {
+//   if (numbers[i] > 0) {
+//     array.push(numbers[i]);
+// }
+// }
+// console.log(array);
+
+// TASK 5
+
