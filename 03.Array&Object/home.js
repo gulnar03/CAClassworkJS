8bd8f3728b3db72c7750929abfac399279747cb9
gulnar03 <gulnarbabakisiyeva@gmail.com>
2023-11-04 01:20:27 -0700

    task finished

---
+++ 03.Array&Object/home.js
@@ -0,0 +1,37 @@
+let arr = [3, 8, 2, 5, 4, 10, 7, 6];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 1) {
+//     console.log(i);
+//   }
+// }
+// for (let i = 0; i < arr.length; i++) {
+//   if ([i] % 2 === 1) {
+//     console.log(arr[i]);
+//   }
+// }
+
+//  number = [];
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] % 2 === 0) {
+//     number.push(arr[i]);
+//   }
+// }
+// console.log(Math.max(...number));
+
+// let num = 10;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === num) {
+//   }
+// }
+// console.log("this number is in array");
+
+let num = [];
+for (let i = 0; i < arr.length; i++) {
+  num.push(arr[i] ** 2);
+}
+console.log(num);
+
+let max = arr[0];
+let mi = a[0];
+for (let i = 0; i < arr.length; i++) {
+}
