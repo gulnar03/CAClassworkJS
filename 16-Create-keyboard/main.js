2e43ced37a867b331c7b72d6274be7c8503ef8bf
gulnar03 <gulnarbabakisiyeva@gmail.com>
2023-11-24 00:51:08 -0800

    task

---
+++ 16-Create-keyboard/main.js
@@ -0,0 +1,13 @@
+let keys = document.querySelectorAll(".key");
+let space = document.querySelector(".words");
+document.addEventListener("keydown", function (event) {
+  keys.forEach((item) => {
+    if (`${item.innerText}` == event.key) {
+      item.style.backgroundColor = "green";
+      item.style.transform = "scale(1.2)";
+    }
+});
+space.innerText += event.key;
+});
+document.addEventListener("keyup",function(event){
+})
